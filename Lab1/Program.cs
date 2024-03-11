8096b4c6fc22ac4105b5660499cdc42fa1f24ede
dimitriiK <dimitriik@whitewave.media>
2024-03-11 06:20:19 +0300

    f

---
+++ Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Buffer to hold a line as it is read in
+            string line;
+            // Loop until no more input (Ctrl-Z in a console, or end-of-file)
+            while ((line = Console.ReadLine()) != null)
+            {
+                // Format the data
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                // Write the results out to the console window
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Constructor for MainWindow
+        /// </summary> 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Read a line of data entered by the user.
+        /// Format the data and display the results in the
+        /// formattedText TextBlock control.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param> 
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            string line = testInput.Text;
+            line = line.Replace(",", " y:");
+            line = "x:" + line;
+            formattedText.Text = line;
+        }
+
+        /// <summary>
+        /// After the Window has loaded, read data from the standard input.
+        /// Format each line and display the results in the
+        /// formattedText TextBlock control.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line + "\n";
+                formattedText.Text += line;
+            }
+        }
+    }
+}
